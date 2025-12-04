f2d57d64df42ee4b444a5a96bed8c44a24984b47
Moussoubi <81056687+Moussoubi@users.noreply.github.com>
2025-12-04 08:25:23 +0000

    tous shainé

--- main.c
+++ main.c
@@ -2,29 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-int saisie() {
+int choix() {
     int x;
     int ok;
 
     do {
-        
         printf("Veuillez faire un choix entre 1 et 4 : ");
-        ok = scanf("%d", &x);
-        
-
-        if (!ok) {
-            printf("Erreur : veuillez entrer un nombre entier !\n");
-
-            
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF);
-
-             if (x < min || x > max) {
-            printf("Erreur : la valeur doit être comprise entre %d et %d.\n", min, max);
-            ok = 0; // pour faire reboucler
+        ok = scanf("%d", &x); 
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        if (ok == 0 || x < 1 || x > 4) {
+            printf("Erreur : veuillez entrer un nombre entier entre 1 et 4 !\n");
+            ok = 0; // Force la continuité de la boucle
+        } else {
+            ok = 1; // La boucle s'arrêtera
         }
-        }
-    } while (!ok);
+
+    } while (ok == 0); 
+    
     return x;
 }
 
@@ -38,7 +33,7 @@ int main() {
         printf(" 2 - Memoryx 1 Joueur 1 Bot \n ");
         printf(" 3 - Mémoryx 2 joueurs \n ");
         printf(" 4 - Quitter le jeu \n ");
-        choix = saisie();
+        choix = choix();
         
             switch (choix) 
             {
